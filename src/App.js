fd65a04cc3a47cd5105a5082ec2d98bdb400b31f
Andreas <me@andreaslorozco.com>
2019-11-17 15:58:22 -0600

    adds lesson 2 code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+const user = {
+  name: "Juana",
+  age: "35"
+};
+
 function App() {
   return (
-    <div className="App">
-      <h1>Lista de Tareas</h1>
+    <div className="container">
+      <h1 className="title">{`${user.name} tiene ${user.age} años de edad`}</h1>
+      <img className="image" alt="" src="https://images.pexels.com/photos/1845534/pexels-photo-1845534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
     </div>
   );
 }
