ca9e2de6692758fc5912306221ba9499eaacaef9
Andreas <me@andreaslorozco.com>
2019-12-31 23:03:46 -0600

    continues lesson 4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const user = {
-  name: "Juana",
-  age: "35"
-};
-
 class App extends Component {
   render() {
+    // una lista con cada una de las tareas a realizar **
+      // cada una de las tareas puede tener:
+        // un boton para marcar la tarea como completada **
+        // un boton para eliminar la tarea **
+    // formulario
+      // una caja de texto para ponerle un titulo a la tarea
+      // un boton para agregar la tarea al la lista
+
     return (
       <div className="container">
-        <h1 className="title">{`${user.name} tiene ${user.age} años de edad`}</h1>
-        <img className="image" alt="" src="https://images.pexels.com/photos/1845534/pexels-photo-1845534.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+         <h1 className="title">Lista de Tareas</h1>
+        <ul>
+          <li>
+            Aprender Javascript
+            <span role="img" aria-label="emoji">
+              🆗
+            </span>
+            <span role="img" aria-label="emoji">
+              ❌
+            </span>
+          </li>
+          <li>
+            Aprender React
+            <span role="img" aria-label="emoji">
+              🆗
+            </span>
+            <span role="img" aria-label="emoji">
+              ❌
+            </span>
+          </li>
+          <li>
+            Suscribirse a Webtoriales
+            <span role="img" aria-label="emoji">
+              🆗
+            </span>
+            <span role="img" aria-label="emoji">
+              ❌
+            </span>
+          </li>
+        </ul>
+        <form>
+          <input type="text" placeholder="Agrega aqui tu tarea"></input>
+          <button>
+            <span  role="img" aria-label="emoji">➕</span>
+          </button>
+        </form>
       </div>
     );
   }
