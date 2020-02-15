b048d762839415060c791b2acd57fd91ce501c66
Andreas <me@andreaslorozco.com>
2020-02-15 12:51:34 -0600

    adds lesson-7

--- src/App.js
+++ src/App.js
@@ -3,48 +3,50 @@ import './App.css';
 
 class App extends Component {
   state = {
-    todoTitle: "Aprender Cocina",
-    todoTitle2: "Aprender Jardineria"
+    todos: [
+      { title: "Aprender Cocina", completed: false },
+      { title: "Aprender React", completed: false },
+      { title: "Suscribirse a Webtoriales", completed: true },
+    ]
   }
 
   handleClick = () => {
     this.setState({ todoTitle: "Aprender Javascript"});
   }
 
+  deleteTodo = e => {
+    const index = e.target.getAttribute("data-index");
+    const todos = [...this.state.todos];
+    todos.splice(index, 1);
+    this.setState({
+      todos: todos
+    });
+  }
+
+  renderTodos = () => {
+    return this.state.todos.map((todo, index) => {
+      return(
+        <li key={todo.title}>
+          <span className="titulo-tarea">{todo.title}</span>
+          <span role="img" aria-label="emoji" className="icono-tarea" onClick={this.handleClick}>
+            🆗
+          </span>
+          <span role="img" aria-label="emoji" className="icono-tarea" onClick={this.deleteTodo} data-index={index}>
+            ❌
+          </span>
+        </li>
+      )
+    });
+  }
+
   render() {
-    console.log(this.state);
+    const renderedTodos = this.renderTodos()
     return (
       <div className="app">
         <div className="lista-tareas-contenedor">
         <h1 className="title">Lista de Tareas</h1>
         <ul>
-          <li>
-            <span className="titulo-tarea">{this.state.todoTitle}</span>
-            <span role="img" aria-label="emoji" className="icono-tarea" onClick={this.handleClick}>
-              🆗
-            </span>
-            <span role="img" aria-label="emoji" className="icono-tarea">
-              ❌
-            </span>
-          </li>
-          <li>
-            <span className="titulo-tarea">Aprender React</span>
-            <span role="img" aria-label="emoji" className="icono-tarea">
-              🆗
-            </span>
-            <span role="img" aria-label="emoji" className="icono-tarea">
-              ❌
-            </span>
-          </li>
-          <li>
-            <span className="titulo-tarea">Suscribirse a Webtoriales</span>
-            <span role="img" aria-label="emoji" className="icono-tarea">
-              🆗
-            </span>
-            <span role="img" aria-label="emoji" className="icono-tarea">
-              ❌
-            </span>
-          </li>
+          {renderedTodos}
         </ul>
         <form>
           <input type="text" placeholder="Agrega aqui tu tarea"></input>
